9744776d750be94ffd7ce238fde25b3ced86c442
Adrien Martinet <contact.adrien.martinet@gmail.com>
2021-12-08 10:38:38 +0100

    Add fake api response and note type

--- README.md
+++ README.md
@@ -10,12 +10,16 @@ Here are the goals for today :
 + create the component for the notes list, for the note display
 + create a service to handle the state of the notes (list, current, add, remove)
 + Use the notes in the ts file provided
-+ *Preferred* : Use behavior subject to display the currently displayed note (You can add a state to ask to click on a note if there is none, or choose to display by default the first one, as you prefer)
++ there is a note type, you can use it and if you want to modify it, or the provided values, go ahead there is no restrictions
++ *Preferred* : Use behavior subject to display the currently displayed note (You can add a state to ask to click on a note if there is none selected, or choose to display by default the first one, as you prefer)
 + Add navigation on click in the note list and next / prev buttons
 + If you have remaining time :
   - add a delete button to delete a note in the list
   - add an add button to add a note
 
+To be more handy we have an api/notes.ts file to simulate a server response. You can just import the file, if you had time to add notes, it's ok if it doesn't save the notes when you refresh, it's not a real project
+If you want to add an interface element to add notes, it's alright if you don't use angular routing I haven't designed this quickie with routes the goal was more to use BehaviorSubject and Subscription from rxjs as we use it in some of our projects 😊
+If you chose a different approach because you never worked with behavior subject and think it might be too short to learn how it works it's ok there is a lot of ways to achieve the same result
 
 Don't worry if everything is not complete by the end of the hour
 Have fun, see you soon 🎉
---
+++ notes/src/app/api/notes.ts
@@ -0,0 +1,42 @@
+import { Note } from "../shared/type/note"
+
+/**
+ * I used https://www.random.org/testimonials/selection-sampling
+ * To generate rondom content
+*****************************/
+
+export const notes: Note[] = [
+  {
+    title: 'Adjudicating Arguments Between Kids',
+    content: 'I find myself turning to the random.org android app most frequently to adjudicate arguments between my son and daughters over whose turn it is to do what and in what order.',
+    creationDate: '17/02/2017'
+  },{
+    title: 'Gathering Review Data',
+    content: 'We found Random.org when we were searching for a way to gather unbiased review data when publishing our articles. We use the service all the time to sample reviews and reviewers in order to gather genuine feedback about the products before we write about them on our website. Thanks for the great service!',
+    creationDate: '01/04/2017'
+  },{
+    title: 'Audiophile and Other Uses',
+    content: "We found Random.org through audiophile channels but have found so many other uses. We've turned hedge fund managers on to some of the tools. We've sent our staff acoustician the ‘white noise’ files which he seemingly uses. On a more simplistic level, we are using the random number generator to settle small bets in the office.",
+    creationDate: '27/06/2017'
+  },{
+    title: '7th Grade Problem Assignment',
+    content: 'Your project is wonderful; thank you for making it free and flexible for public use. I use it every day in my 7th grade classroom to assign warm-up problems to students in my class; this particular crop of kids is highly competitive and I was having trouble selecting from a group with all their hands up!',
+    creationDate: '26/02/2018'
+  },{
+    title: '7th Grade Problem Assignment part 2',
+    content: "I read your FAQ and I wanted to contribute a little thought: to a computer, would it think it has free will? We store and process information in the same way; so we are running a big program which makes decisions based upon lots and lots of current states and simultaneous inputs. I suspect it is impossible to know whether we have free will from within ourselves and will have to use lots of expensive computer modeling to answer the question, but I'm guessing the answer is no.",
+    creationDate: '09/08/2018'
+  },{
+    title: 'Choosing the Right Number of Wolves',
+    content: "I have never been a very decisive person especially when it came to the more important decisions in life. Just recently I was shopping an online retailer in hopes of finding a wolf shirt, and ran into a decision making dilemma. You see, I am a huge fan of wolf shirts for the obvious reason that they speak volumes about a person from a single glance, personality (lone wolf), social habits (enjoys the night life), special talents (casting/bow staff handling). Said shirts, however, have a variable number of wolves howling at the moon, and I was not about to commit the major fashion faux pas of donning the wrong number this late in my 30's.",
+    creationDate: '01/05/2019'
+  },{
+    title: 'Choosing the Right Number of Wolves part 2',
+    content: 'My first thought was to graph the number of wolves vs my percieved irresistibility but that ended up with a 2-way tie between 4 and 5 wolves. Neither my mother (who has always said that I am fairly handsome) nor my WOW guild were able to provide a convincing argument one way or the other, but I lucked out, and stumbled across your website which guaranteed me unparalleled randomness for breaking these kinds of deadlocks.',
+    creationDate: '19/06/2019'
+  },{
+    title: 'Choosing the Right Number of Wolves part 3',
+    content: 'With a click the decision was made and at that point I took a deep breath of musty basement and literally jumped for joy at the new prospects that lay ahead of me in my life rattling my cat elf-fire. To make a long story short your site enabled me to buy the optimal wolf shirt, and my social life has blossomed ever since.',
+    creationDate: '21/12/2020'
+  }
+]
